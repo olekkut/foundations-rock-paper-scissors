cffb358b9c7e2a07652bade5f93b657d0ed17450
Aleksander Kutycki <74899889+olekkut@users.noreply.github.com>
2025-08-13 20:53:51 +0200

    Implement computer choice logic and randomness check for rock-paper-scissors game

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log("Hello, World!");
+function getComputerChoice() {
+    let RandomNumb = Math.random();
+    if (RandomNumb <= 1/3) {
+        return "rock";
+    } else if (RandomNumb <= 2/3 && RandomNumb >= 1/3) {
+        return "paper";
+    } else if (RandomNumb <= 1 && RandomNumb >= 2/3) {
+        return "scissors"
+    }
+}
+
+
+function checkRandomness(){
+    let rock = 0;
+    let paper = 0;
+    let scissors = 0;
+
+    for (let i = 0; i < 100; i++) {
+        let RandomChoice = getComputerChoice();
+        if (RandomChoice === "rock") {
+            rock++;
+        }
+        if (RandomChoice === "paper") {
+            paper++;
+        }
+        if (RandomChoice === "scissors") {
+            scissors++;
+        }  
+    }
+    console.log(rock, paper, scissors);
+    if (rock + paper + scissors === 100) {
+        console.log("The random function is working correctly.");
+    } else {
+        console.log("The random function is not working correctly.");
+    }
+}
+
+checkRandomness();
